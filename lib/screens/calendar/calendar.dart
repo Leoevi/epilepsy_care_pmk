42073284de267a8ca12e9a4e30ebe20faa2ed617
reizyy <103626976+reizyy@users.noreply.github.com>
2024-02-05 11:41:53 +0700

    Insert Calendar Table to Calandar Page

--- lib/screens/calendar/calendar.dart
+++ lib/screens/calendar/calendar.dart
@@ -1,21 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatelessWidget {
-  const Calendar({Key? key}) : super(key: key);
+class Calendar extends StatefulWidget {
+  const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          "This is Calendar",
-          style: TextStyle(
-            color: Colors.green[900],
-            fontSize: 45,
-            fontWeight: FontWeight.w500,
+    return Scaffold(
+      
+      body: content(),
+      backgroundColor: Colors.transparent,
+    );
+  }
+
+  Widget content() {
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border:Border.all(
+              color: Colors.black,
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(10.0)
+          ),
+          padding: const EdgeInsets.all(16.0),
+          margin: const EdgeInsets.all(8.0),
+          child: TableCalendar(
+            calendarStyle: CalendarStyle(
+            
+            ),
+            calendarBuilders: CalendarBuilders(),
+            focusedDay: today,
+            rowHeight: 50,
+            headerStyle: const HeaderStyle(
+                formatButtonVisible: false, titleCentered: true),
+            availableGestures: AvailableGestures.all,
+            selectedDayPredicate: (day) => isSameDay(day, today),
+            firstDay: DateTime.utc(2020, 1, 14),
+            lastDay: DateTime.utc(2030, 3, 14),
+            onDaySelected: _onDaySelected,
           ),
         ),
-      ),
+      ],
     );
   }
 }
+
+
+
+
+//  @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Calendar")),
+//       body: content(),
+//     );
+//   }
+

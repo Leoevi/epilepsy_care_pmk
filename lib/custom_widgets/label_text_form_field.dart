1dc5730261b17669742877bfa8db3b497d06d6dc
Leoevi <76988579+Leoevi@users.noreply.github.com>
2024-02-11 08:44:12 +0700

    modified LabelTextFormField

---
+++ lib/custom_widgets/label_text_form_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+// This is an introduction to write a StatefulWidget class: https://stackoverflow.com/a/72322408
+
+/// A [TextFormField] with hints that is labeled at the top
+class LabelTextFormField extends StatefulWidget {
+  const LabelTextFormField({
+    super.key,
+    required this.label,
+    this.hintText,
+    this.onChanged,
+    this.validator,
+  });
+
+  /// The label above the [TextFormField]
+  final String label;
+
+  final String? hintText;
+
+  /// Callback function that will get executed when the value in the [TextFormField]
+  /// is changed
+  final Function(String val)? onChanged;
+
+  /// A validator function. In order for this to work, you need to
+  /// wrap this widget with a Form ancestor with a key assigned to it
+  /// (https://docs.flutter.dev/cookbook/forms/validation)
+  final String? Function(String? x)? validator;
+
+  @override
+  State<LabelTextFormField> createState() => _LabelTextFormFieldState();
+}
+
+class _LabelTextFormFieldState extends State<LabelTextFormField> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(widget.label, style: TextStyle(fontSize: 18)),
+        SizedBox(height: 10),
+        //spacing between label and TextInput
+        TextFormField(
+          //Collect data by use update_text function
+          onChanged: widget.onChanged,
+          validator: widget.validator,
+          decoration: InputDecoration(
+              hintText: widget.hintText,
+              border: OutlineInputBorder()
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/custom_widgets/time_input.dart
+++ lib/custom_widgets/time_input.dart
@@ -1,16 +1,90 @@
 import 'package:flutter/material.dart';
 
+// Didn't use because kept getting LateInitializationError
 class TimeInput extends StatefulWidget {
-  const TimeInput({super.key});
+  TimeInput({
+    super.key,
+    required this.onSelected,
+  });
+
+  final void Function(TimeOfDay) onSelected;
+
+  TimeOfDay? selectedTime;
 
   @override
   State<TimeInput> createState() => _TimeInputState();
 }
 
 class _TimeInputState extends State<TimeInput> {
+  // To run exec some statements once, do it with initState
+  // (https://stackoverflow.com/questions/61032975/how-to-ensure-code-is-run-only-once-in-a-widget)
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      widget.selectedTime = TimeOfDay.now();
+    });
+  }
+
+  // This widget has one minor bug, when hot reload, it'll cause
+  // late initialization error
+  // @override
+  // void didUpdateWidget(TimeInput) {
+  //   super.didUpdateWidget(TimeInput);
+  //   widget.selectedTime = TimeOfDay.now();
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("โปรดกรอกเวลา", style: TextStyle(fontSize: 18)),
+
+        SizedBox(height: 10),
+
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Expanded(
+              child: TextFormField(
+                //Collect data by use update_text function
+                enabled: false,
+                decoration: InputDecoration(
+                    hintText: widget.selectedTime!.format(context),
+                    // TODO: Change PM to 12-hour (https://stackoverflow.com/questions/70991271/flutter-dart-timeofday-object-returns-timeofday1500-instead-of-only-the-time)
+                    border: OutlineInputBorder()),
+              ),
+            ),
+            SizedBox(width: 80),
+            ElevatedButton(
+              child: Text("เลือกเวลา",
+                  style: TextStyle(
+                      fontSize: 16, color: Colors.white)),
+              onPressed: () async {
+                TimeOfDay? input =
+                await showTimePicker(
+                  context: context,
+                  initialTime: widget.selectedTime!,
+                  initialEntryMode: TimePickerEntryMode.dial,
+                );
+                setState(() {
+                  widget.selectedTime = input ?? widget.selectedTime;  // Null checking syntax instead of ternary
+                });
+                widget.onSelected(widget.selectedTime!);
+              },
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(
+                      255, 201, 128, 247), //Color(0x7FCA80F7)
+                  padding: EdgeInsets.all(20),
+                  fixedSize: Size.fromWidth(140),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+            )
+          ],
+        ),
+      ],
+    );
   }
 }
 

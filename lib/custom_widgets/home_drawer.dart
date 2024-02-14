d9b6c1e374879828dc8e638c6807c5123a78f303
Leoevi <76988579+Leoevi@users.noreply.github.com>
2024-02-14 08:33:14 +0700

    fill in the drawer a bit

--- lib/custom_widgets/home_drawer.dart
+++ lib/custom_widgets/home_drawer.dart
@@ -5,8 +5,46 @@ class HomeDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Drawer(
-      child: Text("test")
+    return Drawer(
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            leading: Icon(Icons.person_2_outlined),
+            title: const Text('ข้อมูลส่วนตัว'),
+            // visualDensity: VisualDensity(vertical: -3),  // Making the tile more compact
+            onTap: () {
+              // Navigator.of(context).push(MaterialPageRoute(
+              //                             builder: (context) => const ???()));
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.data_thresholding_outlined),
+            title: const Text('ประวัติการชักเเละการรับประทานยา'),
+            onTap: () {
+              // Navigator.of(context).push(MaterialPageRoute(
+              //                             builder: (context) => const ???()));
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.alarm),
+            title: const Text('ตั้งเวลากินยา'),
+            onTap: () {
+              // Navigator.of(context).push(MaterialPageRoute(
+              //                             builder: (context) => const ???()));
+            },
+          ),
+        ],
+      ),
     );
   }
 }

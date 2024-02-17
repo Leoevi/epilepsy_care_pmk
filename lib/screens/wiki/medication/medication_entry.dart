75317536f9a5728a0e7eac43f32dd1c7096d20c5
Leoevi <76988579+Leoevi@users.noreply.github.com>
2024-02-17 21:02:30 +0700

    add medicationIntakeMethod to MedicationEntry

--- lib/screens/wiki/medication/medication_entry.dart
+++ lib/screens/wiki/medication/medication_entry.dart
@@ -4,7 +4,7 @@ class MedicationEntry {
   const MedicationEntry({
     required this.name,
     required this.picture,
-    this.dosage,
+    required this.medicationIntakeMethod,
     required this.sideEffects,
     required this.dangerSideEffects,
     this.allergySymptoms,
@@ -12,19 +12,54 @@ class MedicationEntry {
 
   final String name;
   final ImageProvider<Object> picture;
-  final String? dosage;
+  final MedicationIntakeMethod medicationIntakeMethod;
   final String sideEffects;
   final String dangerSideEffects;
   final String? allergySymptoms;
-  // unit String
 }
 
-// class
+/// An abstract class that tries to describe how each medication
+/// is taken. By itself, this is just to dictate that each method
+/// must have a way to return the amount of medication taken in mg
+/// but bears no implementation detail.
+abstract class MedicationIntakeMethod {
+  const MedicationIntakeMethod();  // IDK, but adding this line allows the subclasses to have a const constructor, so here it is
+  double getMg(double unit);
+}
+/// A class that describes medication that is pill-like, as in a discrete unit
+/// of medication that can be eaten whole.
+class PillMedication extends MedicationIntakeMethod {
+  const PillMedication(this.mgPerUnit);
+  final int mgPerUnit;
+
+  @override
+  double getMg(double unit) {
+    return mgPerUnit*unit;
+  }
+}
+/// A class that describes medication that is in liquid form, which must be
+/// taken in terms of millilitres, this introduces many ways to measure how much
+/// the medications is taken, like a teaspoon, or a tablespoon.
+class LiquidMedication extends MedicationIntakeMethod {
+  const LiquidMedication(this.mgPerMl);
+  final int mgPerMl;
+  // This is according to metric standards
+  // (US teaspoon is 4.92892159375, metric teaspoon is 5 ml)
+  // https://en.wikipedia.org/wiki/Teaspoon#Unit_of_measure
+  static const mlPerTeaspoon = 5;
+  static const mlPerTablespoon = 3*mlPerTeaspoon;
+
+  @override
+  double getMg(double unit) {
+    return mgPerMl*unit;
+  }
+}
 
 final List<MedicationEntry> medicationEntries = [
   const MedicationEntry(
       name: "Carbamazepin / Tegretal®",
       picture: AssetImage("image/medications/drug_1.jpg"),
+      medicationIntakeMethod: LiquidMedication(20),
       sideEffects: "คลื่นไส้ ซึม เดินเซ เห็นภาพซ้อน",
       dangerSideEffects: "Hyponatremia (SIADH), Aplastic anemia, ตับอักเสบ เม็ดเลือดขาวต่ำ",
       allergySymptoms: "skin rash, Steven Johnson syndrome*"
@@ -32,79 +67,91 @@ final List<MedicationEntry> medicationEntries = [
   const MedicationEntry(
       name: "Clonazepam / Rivotril®",
       picture: AssetImage("image/medications/drug_2.png"),
+      medicationIntakeMethod: PillMedication(2),
       sideEffects: "อ่อนเพลีย ง่วง พฤติกรรมเปลี่ยนแปลง น้ำลายและเสมหะมาก",
       dangerSideEffects: "กดการหายใจ",
   ),
   const MedicationEntry(
       name: "Lamotrigine / Lamictal®",
       picture: AssetImage("image/medications/drug_3.png"),
+      medicationIntakeMethod: PillMedication(25),
       sideEffects: "มึนงง เห็นภาพซ้อน เดินเซ",
       dangerSideEffects: "ผื่น Stevens-Johnson syndrome",
   ),
   const MedicationEntry(
-    name: "Levetiracetam /  Keppra®",
+    name: "Levetiracetam / Keppra®",
     picture: AssetImage("image/medications/drug_4.png"),
+    medicationIntakeMethod: LiquidMedication(100),
     sideEffects: "ซึม มึนงง",
     dangerSideEffects: "อารมณ์หงุดหงิด ก้าวร้าว",
   ),
   const MedicationEntry(
     name: "Oxcarbazepine / Trileptal®",
     picture: AssetImage("image/medications/drug_5.png"),
+    medicationIntakeMethod: PillMedication(300),
     sideEffects: "มึนงง ง่วงซึม เดินเซ",
     dangerSideEffects: "ภาวะโซเดียมต่ำ",
   ),
   const MedicationEntry(
     name: "Phenobarbital",
     picture: AssetImage("image/medications/drug_6.png"),
+    medicationIntakeMethod: PillMedication(30),
     sideEffects: "เด็ก: ซุกซนไม่อยู่สุข พฤติกรรมเปลี่ยนแปลง ก้าวร้าว ผู้ใหญ่: ง่วงซึม อ่อนเพลีย",
     dangerSideEffects: "ผื่น Stevens-Johnson syndrome",
   ),
   const MedicationEntry(
-    name: "Phenytoin / Dilantin®",
+    name: "Phenytoin / Dilantin® (125 มก./5 มล.)",
     picture: AssetImage("image/medications/drug_7.png"),
+    medicationIntakeMethod: LiquidMedication(25),
     sideEffects: "เวียนศีรษะ เห็นภาพซ้อน ซึม เดินเซ คลื่นไส้ อาเจียน เหงือกบวม หน้าหยาบ ขนเยอะ สิวเพิ่มขึ้น",
     dangerSideEffects: "ผื่น Stevens-Johnson syndrome ตับอักเสบ",
   ),
   const MedicationEntry(
     name: "Sodium valproate/ Depakin®",
-    dosage: "25 mg",
     picture: AssetImage("image/medications/drug_8.png"),
+    medicationIntakeMethod: LiquidMedication(200),
     sideEffects: "มือสั่น คลื่นไส้ อาเจียนปวดท้อง ผมร่วง น้ำหนักเพิ่ม",
     dangerSideEffects: "ตับอักเสบ ตับอ่อนอักเสบ",
   ),
   const MedicationEntry(
     name: "Topiramate / Topamax®",
     picture: AssetImage("image/medications/drug_9.png"),
+    medicationIntakeMethod: PillMedication(25),
     sideEffects: "มึนงง เดินเซ ความคิดเชื่องช้า การพูดผิดปกติ น้ำหนักลด",
     dangerSideEffects: "นิ่วในไต ต้อหิน เหงื่อออกน้อย",
   ),
   const MedicationEntry(
     name: "Vigabatrin / Sabril®",
     picture: AssetImage("image/medications/drug_10.png"),
+    medicationIntakeMethod: PillMedication(500),
     sideEffects: "มึนงง ง่วงซึม",
     dangerSideEffects: "ความผิดปกติของลานสายตา",
   ),
   const MedicationEntry(
     name: "Perampanel / Fycompa®",
     picture: AssetImage("image/medications/drug_11.png"),
+    medicationIntakeMethod: PillMedication(4),
     sideEffects: "มึนงง ง่วงซึม เดินเซ",
     dangerSideEffects: "หงุดหงิด ก้าวร้าว อาการทางจิต",
   ),
   const MedicationEntry(
     name: "Lacosamide / Vimpat®",
     picture: AssetImage("image/medications/drug_12.png"),
+    medicationIntakeMethod: PillMedication(50),
     sideEffects: "มึนงง ง่วงซึม ภาพซ้อน เดินเซ",
     dangerSideEffects: "Atrioventricular block, palpitation",
   ),
   const MedicationEntry(
-    name: "Pregabalin / Lyrica®",
+    name: "Pregabalin / Lyrica® (25 มก.)",
     picture: AssetImage("image/medications/drug_13.png"),
+    medicationIntakeMethod: PillMedication(25),
     sideEffects: "ง่วงนอน ซึม เวียนศีรษะ",
     dangerSideEffects: "มักไม่ค่อยพบ",
   ),
   const MedicationEntry(
     name: "Gabapentin / Neurontin® / Berlontin®",
     picture: AssetImage("image/medications/drug_14.png"),
+    medicationIntakeMethod: PillMedication(300),
     sideEffects: "ง่วงนอน ซึม เวียนศีรษะ",
     dangerSideEffects: "มักไม่ค่อยพบ",
   ),
